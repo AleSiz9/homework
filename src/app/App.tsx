392b1885ec40809145f61d833e0a8850064dd890
Alex Sus <sizikov_1998@inbox.ru>
2026-03-10 20:09:52 +0500

    first

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import PostList from '@/widgets/PostList/PostList'
+
+
+function App() {
+
+  return (
+    <>
+      <PostList />
+    </>
+  )
+}
+
+export default App
